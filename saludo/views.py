d39523c75d16e6d40a8af1a4e21962915dffcd06
mchena <m.chena@itecriocuarto.org.ar>
2026-04-08 23:06:03 -0300

    cambio de contexto mascotas por tareas

--- saludo/views.py
+++ saludo/views.py
@@ -1,25 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todolist.models import Tarea
 
 # En views va toda la logica de la aplicacion
 
-# Le pasamos variables al html
-contexto = {
-    "nombre": "Mundo",
-    "esMayor": True,
-    "mascotas": ["Perro", "Gato", "Pez"]
-}
-
 def saludo(request):
+    tareas = Tarea.objects.all()
 
     # Renderiza un html con el saludo
-    return render(request, "saludo/saludo.html", contexto)
+    return render(request, "saludo/saludo.html", {"tareas": tareas})
 
 def despedir(request):
 
     # Renderiza un html con la despedida
-    return render(request, "saludo/despedir.html", contexto)
+    return render(request, "saludo/despedir.html")
 
 def inicio(request):
     # Renderiza un html con el inicio
-    return render(request, "saludo/inicio.html", contexto)
+    return render(request, "saludo/inicio.html")
