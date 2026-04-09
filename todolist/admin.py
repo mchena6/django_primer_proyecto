60f3e6bb3915254f6882fb768f0ea337c556d485
mchena <m.chena@itecriocuarto.org.ar>
2026-04-08 23:04:32 -0300

    personalizacion de tabla tarea

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Tarea
+from .models import Tarea, Etiqueta
 
 
 # Register your models here.
-admin.site.register(Tarea)
+admin.site.register(Etiqueta)
+
+# Funcion para personalizar el admin de tareas
+@admin.register(Tarea)
+class TareaAdmin(admin.ModelAdmin):
+    # Columnas
+    list_display = ("id", "nombre", "completada", "fecha_completado","fecha_creacion", "responsable")
+    # Columna para filtrar
+    list_filter = ("completada",)
+    # Barra de busqueda
+    search_fields = ("nombre",)
+    # Campos que no se pueden modificar
+    readonly_fields = ("completada",)
