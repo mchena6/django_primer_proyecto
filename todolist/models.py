a63e35609efdfcc0a3a2013d3cd0c24541bb8234
mchena <m.chena@itecriocuarto.org.ar>
2026-04-08 23:05:04 -0300

    modelo de etiqueta agregado

--- todolist/models.py
+++ todolist/models.py
@@ -1,13 +1,41 @@
 from django.db import models
-
-# Create your models here.
+# Importar usuarios
+from django.contrib.auth.models import User
 
 # El id se crea automaticamente
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+    color = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Soy la etiqueta: {self.nombre}"
+
 class Tarea(models.Model):
     nombre = models.CharField(max_length=100)
-    completada = models.BooleanField(default=False)
+    completada = models.BooleanField(
+        default=False,
+        help_text="La tarea esta completada?",
+        verbose_name="Tarea finalizada"    
+    )
     fecha_completado = models.DateField()
     fecha_creacion = models.DateTimeField(auto_now=True)
+    # Campo relacionado a un usuario
+    responsable = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="responsable", 
+        default=None, 
+        blank=True,
+        null=True
+        )
+    # Para ver el responsable de una tarea, se puede usar: tarea.responsable
+    etiqueta = models.ManyToManyField(
+        Etiqueta, 
+        related_name="etiqueta", 
+        default=None, 
+        blank=True)
+
 
     def __str__(self):
         return f"Soy la tarea: {self.nombre}"
