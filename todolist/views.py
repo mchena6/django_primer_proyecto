ae30cf9ea6880c17a16b40beb6506f9b6e0a5156
mchena <m.chena@itecriocuarto.org.ar>
2026-04-14 23:29:56 -0300

    vista para crear tareas

--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Tarea
+from .forms import TareaForm
 
 # Create your views here.
 
 def todolist(request):
-    #tareas = Tarea.objects.all()
-    #tareas = Tarea.objects.first()
-    tareas = Tarea.objects.filter(completada=False)
-    return HttpResponse(tareas)
+    tareas = Tarea.objects.all().order_by('-id')
+    
+    return render (request, 'todolist/index.html', {'tareas': tareas})
+
+def crear_tarea(request):
+    if request.method == "POST":
+        # Mandar los datos del formulario 
+        form = TareaForm(request.POST)
+        # Verificar que los campos son validos
+        if form.is_valid():
+            # Guardar en la base de datos
+            form.save()
+            return redirect('tareas')
+    else:
+        # Renderizar el formulario vacio
+        form = TareaForm()
+
+    return render(request, 'todolist/crear_tarea.html', {'form': form})
