a5369a132a7ee7dd2d23e53b566535f449872ca1
mchena <m.chena@itecriocuarto.org.ar>
2026-04-21 23:07:57 -0300

    funciones para editar y borrar tareas agregados

--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarea
 from .forms import TareaForm
 
-# Create your views here.
 
 def todolist(request):
-    tareas = Tarea.objects.all().order_by('-id')
+    tareas = Tarea.objects.filter(activo=True).order_by('-id')
     
     return render (request, 'todolist/index.html', {'tareas': tareas})
 
@@ -23,3 +22,29 @@ def crear_tarea(request):
         form = TareaForm()
 
     return render(request, 'todolist/crear_tarea.html', {'form': form})
+
+# Parametro Ruta: url.com/editar_tarea/<id>
+def editar_tarea(request, id):
+    # Obtener tarea
+    tarea = get_object_or_404(Tarea, id=id)
+    
+    if request.method == "POST":
+        form = TareaForm(request.POST, instance=tarea)
+        if form.is_valid():
+            form.save()
+            return redirect('tareas')
+    else:   
+        # Renderizar el formulario con los datos de la tarea
+        form = TareaForm(instance=tarea)
+    return render(request, "todolist/editar_tarea.html", {"form": form})
+
+def eliminar_tarea(request, id):
+    tarea = get_object_or_404(Tarea, id=id)
+    
+    # Borrado logico
+    if request.method == "POST":
+        tarea.activo = False
+        # Guardar el cambio en la base de datos
+        tarea.save()
+        return redirect('tareas')
+    return render(request, "todolist/borrar_tarea.html", {"tarea": tarea})
