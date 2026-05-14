6cecf6e476ea0f56983bee9ba5971ca664c6632f
mchena <m.chena@itecriocuarto.org.ar>
2026-05-14 20:57:21 -0300

    Actualización de URLs

--- todolist/urls.py
+++ todolist/urls.py
@@ -8,9 +8,8 @@ from . import views
 # Variable para conectar las rutas con las vistas
 # Ruta, view, nombre de la ruta para usarla en el html
 urlpatterns = [
-    path('', views.todolist, name="tareas"),
+    path("", views.todolist, name="tareas"),
     path("nueva/", views.crear_tarea, name="crear_tarea"),
     path("editar/<int:id>/", views.editar_tarea, name="editar_tarea"),
     path("borrar/<int:id>/", views.eliminar_tarea, name="borrar_tarea"),
-    path("register/",views.registrarse, name="register")
 ]
